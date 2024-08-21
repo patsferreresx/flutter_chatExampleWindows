31dc2f32aa4bdb3f4cb08b97f2fb8e77add2f234
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-08-21 16:04:57 -0300

    responsividade do messageBox

--- lib/widget/messageBox.dart
+++ lib/widget/messageBox.dart
@@ -9,95 +9,101 @@ class MessageBox extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Scouting Group",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              SizedBox(height: 50),
-              Text(
-                "Welcome to Stream line scouting chat",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.blue,
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                "We can now freely collaborate regarding our current demand\n"
-                "Any question about the documentation or the project\n"
-                "please feel free to get in contact us\n",
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.w300,
-                ),
-              ),
-              SizedBox(height: 30),
-              Align(
-                alignment: Alignment.center,
-                child: Text(
-                  "Tuesday, April 7th at 1:21PM",
-                  style: TextStyle(
-                      fontWeight: FontWeight.w300,
-                      color: Theme.of(context).iconTheme.color),
-                ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                child: Row(
+          Expanded(
+            child: Container(
+              child: SingleChildScrollView(
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    CircleAvatar(),
-                    SizedBox(width: 10),
-                    Flexible(
-                      child: Column(
+                    Text(
+                      "Scouting Group",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    Text(
+                      "Welcome to Stream line scouting chat",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "We can now freely collaborate regarding our current demand\n"
+                      "Any question about the documentation or the project\n"
+                      "please feel free to get in contact us\n",
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Tuesday, April 7th at 1:21PM",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            color: Theme.of(context).iconTheme.color),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      child: Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          _buildMessage(
-                              "Oi.. sou o ChatUs, como você se 100te hoxe? Turu pom?"),
-                          _buildMessage(
-                              "Estou te mandando isso pq estou vendo que vc tá vendo live de Valorant faz 2h"),
-                          _buildMessage(
-                              "Vai trabalhar vagabol akakaka com todo respeitum rs"),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Flexible(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          _buildMessage(
-                            "Opa lindãum, esqueci a aba aberta sem querer heehe ",
-                            isSend: true,
-                          ),
-                          _buildMessage(
-                            "não tô vendo nada D: é youtube de código",
-                            isSend: true,
-                          ),
+                          CircleAvatar(),
+                          SizedBox(width: 10),
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                _buildMessage(
+                                    "Oi.. sou o ChatUs, como você se 100te hoxe? Turu pom?"),
+                                _buildMessage(
+                                    "Estou te mandando isso pq estou vendo que vc tá vendo live de Valorant faz 2h"),
+                                _buildMessage(
+                                    "Vai trabalhar vagabol akakaka com todo respeitum rs"),
+                              ],
+                            ),
+                          )
                         ],
                       ),
                     ),
-                    SizedBox(width: 10),
-                    CircleAvatar(),
+                    Container(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                _buildMessage(
+                                  "Opa lindãum, esqueci a aba aberta sem querer heehe ",
+                                  isSend: true,
+                                ),
+                                _buildMessage(
+                                  "não tô vendo nada D: é youtube de código",
+                                  isSend: true,
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          CircleAvatar(),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
-            ],
+            ),
           ),
-          Spacer(),
+          SizedBox(height: 20),
           Container(
             child: Row(
               children: [
