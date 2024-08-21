5b81d362c8f4b740fa008cff11790732429f2c27
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-08-21 16:05:10 -0300

    responsividade do more_details

--- lib/widget/more_details.dart
+++ lib/widget/more_details.dart
@@ -7,129 +7,133 @@ class MoreDetails extends StatelessWidget {
     return DefaultTabController(
       length: 2,
       child: Container(
-        child: Column(
-          children: [
-            TabBar(
-              labelColor: Theme.of(context).primaryColor,
-              unselectedLabelColor: Colors.grey[700],
-              indicatorColor: Theme.of(context).primaryColor,
-              tabs: [
-                Tab(text: "Chat Members"),
-                Tab(text: "Shared Files"),
-              ],
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Members".toUpperCase(),
-                    style: TextStyle(
-                      color: Theme.of(context).iconTheme.color,
-                      fontSize: 15,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  SizedBox(height: 30),
-                  Column(
-                    children: List.generate(
-                      3,
-                      (index) => MemberCard(
-                        showJob: true,
-                        member: members[index],
-                        trailing: Container(
-                          child: Material(
-                            borderRadius: BorderRadius.circular(10),
-                            color:
-                                Theme.of(context).primaryColor.withOpacity(.1),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: IconButton(
-                                color: Theme.of(context).primaryColor,
-                                icon: Icon(Icons.message),
-                                onPressed: () {},
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Divider(),
-                  SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Text(
-                        "Admins".toUpperCase(),
-                        style: TextStyle(
-                          color: Theme.of(context).iconTheme.color,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      Spacer(),
-                      IconButton(
-                        icon: Icon(Icons.edit_outlined),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Column(
-                    children: List.generate(
-                      1,
-                      (index) => MemberCard(
-                        showJob: true,
-                        member: members[index],
-                        trailing: Container(
-                          child: Material(
-                            borderRadius: BorderRadius.circular(10),
-                            color:
-                                Theme.of(context).primaryColor.withOpacity(.1),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: IconButton(
-                                color: Theme.of(context).primaryColor,
-                                icon: Icon(Icons.message),
-                                onPressed: () {},
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 30),
-                  ListTile(
-                    title: Text(
-                      "Customize Chat",
-                      style: TextStyle(
-                          color: Color(0xFF78A1C6),
-                          fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text(
-                      "Change layout and colors",
-                      style: TextStyle(
-                        color: Color(0xFF78A1C6).withOpacity(.7),
-                      ),
-                    ),
-                  ),
-                  ListTile(
-                    title: Text(
-                      "Privacy and Support",
-                      style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    subtitle: Text("Get immediate support"),
-                  ),
+        child: SingleChildScrollView( // faz scroll
+          child: Column(
+            children: [
+              TabBar(
+                labelColor: Theme.of(context).primaryColor,
+                unselectedLabelColor: Colors.grey[700],
+                indicatorColor: Theme.of(context).primaryColor,
+                tabs: [
+                  Tab(text: "Chat Members"),
+                  Tab(text: "Shared Files"),
                 ],
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Members".toUpperCase(),
+                      style: TextStyle(
+                        color: Theme.of(context).iconTheme.color,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Column(
+                      children: List.generate(
+                        3,
+                        (index) => MemberCard(
+                          showJob: true,
+                          member: members[index],
+                          trailing: Container(
+                            child: Material(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Theme.of(context)
+                                  .primaryColor
+                                  .withOpacity(.1),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: IconButton(
+                                  color: Theme.of(context).primaryColor,
+                                  icon: Icon(Icons.message),
+                                  onPressed: () {},
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Divider(),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Text(
+                          "Admins".toUpperCase(),
+                          style: TextStyle(
+                            color: Theme.of(context).iconTheme.color,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Spacer(),
+                        IconButton(
+                          icon: Icon(Icons.edit_outlined),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Column(
+                      children: List.generate(
+                        1,
+                        (index) => MemberCard(
+                          showJob: true,
+                          member: members[index],
+                          trailing: Container(
+                            child: Material(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Theme.of(context)
+                                  .primaryColor
+                                  .withOpacity(.1),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: IconButton(
+                                  color: Theme.of(context).primaryColor,
+                                  icon: Icon(Icons.message),
+                                  onPressed: () {},
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    ListTile(
+                      title: Text(
+                        "Customize Chat",
+                        style: TextStyle(
+                            color: Color(0xFF78A1C6),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        "Change layout and colors",
+                        style: TextStyle(
+                          color: Color(0xFF78A1C6).withOpacity(.7),
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                        "Privacy and Support",
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text("Get immediate support"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
